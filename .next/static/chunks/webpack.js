20c097984fc98a4472f080cad50b713b73515fb6
EatYourSoup7 <r.kondratowicz1@gmail.com>
2021-03-25 12:15:46 -0500

    pagination setup on guitars page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2cd4a641f328810e0728";
+/******/ 	var hotCurrentHash = "df5ffb5a7d0aabd2e976";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/guitars.js
+++ pages/guitars.js
@@ -1,9 +1,12 @@
 import Guitars from "../components/Guitars";
+import Pagination from "../components/Pagination";
 
 export default function GuitarsPage() {
   return (
     <div>
+      <Pagination page={1} />
       <Guitars />
+      <Pagination page={1} />
     </div>
   )
 }
